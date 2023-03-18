b784dec7667402bab7a4f6baeaaf2b1164cf30a0
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-03-17 21:30:17 -0500

    Healtcheck

--- widmy/urls.py
+++ widmy/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
     path('', include('manejadorPersonalSalud.urls')),
-    path('', include('manejadorHistorias.urls'))
+    path('', include('manejadorHistorias.urls')),
+    path('health-check', views.healthCheck)
 ]
