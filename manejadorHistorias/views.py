1d6c2454b88e19fb98b4a540b56d5af7543dce6e
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-05 23:49:50 -0500

    correccion

--- manejadorHistorias/views.py
+++ manejadorHistorias/views.py
@@ -9,6 +9,7 @@ def historiasList(request):
     role = getRole(request)
     if (role == 'Administrador'):
         context = {'listaHistorias': getHistorias()}
+        print(context)
         return render(request, 'ManejadorHistorias/historias_List.html', context)
     else:
         return HttpResponse("Unauthorized User")
