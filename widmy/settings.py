eb6416b218732e4f50176809287b2bddd83d5e99
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-05 23:26:38 -0500

    correccion

--- widmy/settings.py
+++ widmy/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'widmy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'widmy_db',
         'USER': 'widmy',
         'PASSWORD': 'widmy',
