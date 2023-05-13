a0b33686e1dc6f9761c9c33566eb95612cd9a1d8
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2023-05-12 19:47:43 -0500

    Correcion ip

--- widmy/settings.py
+++ widmy/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = "/login/auth0" 
 LOGIN_REDIRECT_URL = "/" 
-LOGOUT_REDIRECT_URL = "https://widmy-sprint3.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.224.103.58:8080" 
+LOGOUT_REDIRECT_URL = "https://widmy-sprint3.us.auth0.com/v2/logout?returnTo=http%3A%2F%2F35.184.224.25:8000" 
 SOCIAL_AUTH_TRAILING_SLASH = False # Remove end slash from routes 
 SOCIAL_AUTH_AUTH0_DOMAIN = 'widmy-sprint3.us.auth0.com' 
 SOCIAL_AUTH_AUTH0_KEY = 'XElqKwbVtQTdBDBRaEqJpDePmiVmUlsO' 
